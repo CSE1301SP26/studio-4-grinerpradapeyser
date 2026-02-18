9c993744fcc5db772101f791a618f3d43b0d92fb
Sophia Prada Peyser <pradapeyser@wustl.edu>
2026-02-18 14:14:13 -0600

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,36 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		Color fur = new Color(240, 157, 70);
+		StdDraw.setPenColor(fur);
+		StdDraw.filledCircle(0.5, 0.5, 0.35);
+
+		double[] x = {0.15, 0.2, 0.35};
+		double[] y = {0.95, 0.5, 0.8};
+		StdDraw.filledPolygon(x, y);
+
+		double[] x2 = {0.85, 0.8, 0.65};
+		double[] y2 = {0.95, 0.5, 0.8};
+		StdDraw.filledPolygon(x2, y2);
+
+		Color nose = new Color(255, 138, 175);
+		StdDraw.setPenColor(nose);
+		double[] x3 = {0.43, 0.5, 0.57};
+		double[] y3 = {0.5, 0.43, 0.5};
+		StdDraw.filledPolygon(x3, y3);
+
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.setPenRadius(0.085);
+		StdDraw.point(0.38,0.61);
+		StdDraw.point(0.62,0.61);
+
+		StdDraw.setPenRadius(0.01);
+		StdDraw.arc(0.5,0.42,0.1, 200, 340);
+
+		StdDraw.setPenRadius(0.008);
+		StdDraw.line(0.5,0.43,0.5,0.32);
+
+		
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
+import java.awt.Color;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 /**
@@ -20,5 +22,24 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+
+	String name = in.next();
+	int redComponent = in.nextInt();
+	int greenComponent = in.nextInt();
+	int blueComponent = in.nextInt();
+	boolean isFilled = in.nextBoolean();
+	double parameterOne = in.nextDouble();
+	double parameterTwo = in.nextDouble();
+	double parameterThree = in.nextDouble();
+	double parameterFour = in.nextDouble();
+
+	Color color = new Color(redComponent, greenComponent, blueComponent);
+	StdDraw.setPenColor(color);
+
+	if (isFilled) {
+		StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+	} else {
+		StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+	}
 	}
 }
